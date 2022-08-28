676eb86f43e5aa18ce359e700bbf132f3c5e266d
ruanzetao <ruanzetao@gmail.com>
2022-08-28 07:05:41 -0700

    Init Page Object Model

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,37 @@
 package commons;
 
-public class BaseTest {
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+	protected WebDriver driver;
+	private String projectPath = System.getProperty("user.dir");
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driver = new FirefoxDriver();
+		} else {
+			throw new RuntimeException("Browser invalid");
+		}
+
+		// Init basePage
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().window().maximize();
+
+		return driver;
+	}
+
+	public int generateFakeNumber() {
+		Random rand = new Random();
+		return (int) rand.nextInt(9999);
+
+	}
 }
